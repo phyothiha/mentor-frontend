10b6a0f4ea75cc39246ec4743d00b23d53fc3030
Phyo Thiha <phyothiha96@gmail.com>
2025-07-12 09:37:34 +0630

    chore: minimum setup page and layout

---
+++ layouts/Dashboard.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        Layout / Dashboard
+        
+        <slot />
+    </div>
+</template>
---
+++ pages/auth/login.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'auth',
+})
+</script>
+
+<template>
+    <div>
+        Page / Auth / Login
+    </div>
+</template>
